467b0ffedf17b4e9a3e9ab5d5393f83962d7482a
Daniel Kinahan <danielkinahan@uvica.ca>
2023-12-17 15:27:54 -0500

    changed config and added example and dockerfile

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+# Use an official Python runtime as the base image
+FROM python:3.12
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy the requirements file to the container
+COPY requirements.txt .
+
+# Install the Python dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the rest of the application code to the container
+COPY . .
+
+# Run the Python app
+CMD ["python", "import.py"]
--- import.py
+++ import.py
@@ -9,7 +9,7 @@ import time
 import datetime
 import logging
 
-cursor = ""
+cursors = []
 
 
 # Function to read credentials from config.toml file
@@ -17,72 +17,80 @@ cursor = ""
 
 def read_config():
     with open('config.toml', 'r') as file:
-        config = toml.load(file)
-        plaid_config = config.get('plaid', {})
-        if not plaid_config:
-            logging.error("Unable to read plaid config")
-        firefly_config = config.get('firefly', {})
-        if not firefly_config:
-            logging.error("Unable to read firefly config")
-        return plaid_config, firefly_config
+        config_file = toml.load(file)
+        config = config_file.get('config', {})
+        if not config:
+            logging.error("Unable to read config")
+        accounts = config_file.get('accounts', {})
+        if not accounts:
+            logging.error("Unable to read accounts config")
+        return config, accounts
 
 # Function to get new transactions from Plaid. On first run this will return all.
+# For this to work with multiple access tokens, we have an array to store cursors for each token.
 
 
-def plaid_sync_transactions(client, plaid_config):
+def plaid_sync_transactions(client, config):
 
-    global cursor
+    global cursors
 
-    if cursor:
-        request = TransactionsSyncRequest(
-            access_token=plaid_config['access_token'],
-            cursor=cursor
-        )
-    else:
-        request = TransactionsSyncRequest(
-            access_token=plaid_config['access_token'],
-        )
+    for i in range(len(config['plaid_access_tokens'])):
+        if cursors[i]:
+            request = TransactionsSyncRequest(
+                access_token=config['plaid_access_tokens'][i],
+                cursor=cursors[i]
+            )
+        else:
+            request = TransactionsSyncRequest(
+                access_token=config['plaid_access_tokens'][i],
+            )
 
-    response = client.transactions_sync(request)
-    transactions = response['added']
-    cursor = response['next_cursor']
-
-    # Get transactions from Plaid
-    while (response['has_more']):
-        request = TransactionsSyncRequest(
-            access_token=plaid_config['access_token'],
-            cursor=cursor
-        )
         response = client.transactions_sync(request)
-        transactions += response['added']
-        cursor = response['next_cursor']
+        transactions = response['added']
+        cursors[i] = response['next_cursor']
+
+        # Get transactions from Plaid
+        while (response['has_more']):
+            request = TransactionsSyncRequest(
+                access_token=config['plaid_access_tokens'][i],
+                cursor=cursors[i]
+            )
+            response = client.transactions_sync(request)
+            transactions += response['added']
+            cursors[i] = response['next_cursor']
 
     return transactions
 
 
-def firefly_get_existing_transactions(firefly_config):
+def firefly_get_existing_transactions(config, accounts):
 
-    firefly_api_key = firefly_config['api_key']
-    firefly_base_url = firefly_config['base_url']
+    firefly_api_key = config['firefly_api_key']
+    firefly_base_url = config['firefly_base_url']
     headers = {
         'Authorization': f'Bearer {firefly_api_key}',
         'Content-Type': 'application/json'
     }
 
-    url = firefly_base_url + '/api/v1/accounts/' + \
-        firefly_config['account'] + '/transactions'
-    response = requests.get(url, headers=headers).json()
-    transactions = response['data']
-    while (response['links'].get('next')):
-        response = requests.get(
-            response['links']['next'], headers=headers).json()
-        transactions += response['data']
+    for account in accounts.values():
+        url = f"{firefly_base_url}/api/v1/accounts/{account}/transactions"
+        response = requests.get(url, headers=headers).json()
+        transactions = response['data']
+        while (response['links'].get('next')):
+            response = requests.get(
+                response['links']['next'], headers=headers).json()
+            transactions += response['data']
 
     return transactions
 
 
-def clean_transaction_account_name(plaid_config, name):
-    for string in plaid_config['remove_strings']:
+def firefly_filter_for_transaction_ids(firefly_existing_transactions):
+    firefly_ids = {firefly_transaction['attributes']['transactions'][0]['external_id']
+                   for firefly_transaction in firefly_existing_transactions}
+    return firefly_ids
+
+
+def clean_transaction_account_name(config, name):
+    for string in config['remove_strings']:
         name = name.replace(string, '')
     return name
 
@@ -106,7 +114,6 @@ def find_matching_transactions(firefly_existing_transactions, plaid_transaction)
             firefly_transaction['attributes']['transactions'][0]['amount'])
         f_type = firefly_transaction['attributes']['transactions'][0]['type']
 
-        # not quite working just yet
         if p_type == f_type and p_amount == f_amount and p_date == f_date:
             matching.append(
                 firefly_transaction['attributes']['transactions'][0]['transaction_journal_id'])
@@ -114,10 +121,10 @@ def find_matching_transactions(firefly_existing_transactions, plaid_transaction)
     return matching
 
 
-def update_existing_transaction_with_id(firefly_config, firefly_id, plaid_id):
+def update_existing_transaction_with_id(config, firefly_id, plaid_id):
 
-    firefly_api_key = firefly_config['api_key']
-    firefly_base_url = firefly_config['base_url']
+    firefly_api_key = config['firefly_api_key']
+    firefly_base_url = config['firefly_base_url']
     headers = {
         'Authorization': f'Bearer {firefly_api_key}',
         'Content-Type': 'application/json'
@@ -130,71 +137,85 @@ def update_existing_transaction_with_id(firefly_config, firefly_id, plaid_id):
     logging.info(
         f"Firefly transaction {firefly_id} matches plaid transaction {plaid_id}")
 
-    response = requests.put(firefly_base_url + '/api/v1/transactions/' +
-                            firefly_id, headers=headers, data=json.dumps(payload))
+    response = requests.put(
+        f'{firefly_base_url}/api/v1/transactions/{firefly_id}', headers=headers, data=json.dumps(payload))
     if response.status_code == 200:
         logging.info(f"Transaction '{plaid_id}' updated successfully.")
     else:
         logging.error(
             f"Failed to update transaction '{plaid_id}'. Status code: {response.status_code}")
 
+
+def match_transaction(config, transaction, firefly_existing_transactions):
+
+    matching = find_matching_transactions(
+        firefly_existing_transactions, transaction)
+    if len(matching) == 1:
+        update_existing_transaction_with_id(
+            config, matching[0], transaction['transaction_id'])
+        return True
+    elif len(matching) > 1:
+        logging.info("Multiple matches found. Not updating.")
+
+    return False
+
 # Function to insert transactions into Firefly III
 
 
-def insert_transactions(plaid_transactions, firefly_existing_transactions, firefly_config, plaid_config):
+def insert_transactions(config, accounts, plaid_transactions, firefly_existing_transactions):
 
-    firefly_api_key = firefly_config['api_key']
-    firefly_base_url = firefly_config['base_url']
+    firefly_api_key = config['firefly_api_key']
+    firefly_base_url = config['firefly_base_url']
     headers = {
         'Authorization': f'Bearer {firefly_api_key}',
         'Content-Type': 'application/json',
         'Accept': 'application/json'
     }
 
-    firefly_ids = {firefly_transaction['attributes']['transactions'][0]['external_id']
-                   for firefly_transaction in firefly_existing_transactions}
+    firefly_ids = firefly_filter_for_transaction_ids(
+        firefly_existing_transactions)
 
     for transaction in plaid_transactions:
 
         other_account = clean_transaction_account_name(
-            plaid_config, transaction['name'])
+            config, transaction['name'])
 
         if transaction['transaction_id'] in firefly_ids:
             logging.info(
                 f"Transaction '{transaction['name']}' already exists in Firefly. Skipping insertion.")
             continue
 
-        if transaction['account_id'] != plaid_config['account']:
+        if transaction['account_id'] not in accounts.keys():
             continue
 
+        if config['match_transactions']:
+            if match_transaction(config, transaction, firefly_existing_transactions):
+                continue
+
         payload = {
-            'transactions': []
+            'transactions': [{
+                "date": transaction['date'].isoformat(),
+                "description": transaction['name'],
+                "external_id": transaction['transaction_id'],
+                "currency_code": transaction['iso_currency_code'],
+                "tags": transaction['category']
+            }]
         }
 
         # Transaction amount has to be positive number
         if transaction['amount'] < 0:
-            payload['transactions'].append({
+            payload['transactions'][0].update({
                 "type": "deposit",
                 "amount": abs(transaction['amount']),
-                "date": transaction['date'].isoformat(),
-                "description": transaction['name'],
                 "source_name": other_account,
-                "destination_id": firefly_config['account'],
-                "external_id": transaction['transaction_id'],
-                "currency_code": transaction['iso_currency_code'],
-                "tags": transaction['category']
+                "destination_id": accounts[transaction['account_id']],
             })
         else:
-            payload['transactions'].append({
+            payload['transactions'][0].update({
                 "type": "withdrawal",
                 "amount": transaction['amount'],
-                "date": transaction['date'].isoformat(),
-                "description": transaction['name'],
-                "source_id": firefly_config['account'],
+                "source_id": accounts[transaction['account_id']],
                 "destination_name": other_account,
-                "external_id": transaction['transaction_id'],
-                "currency_code": transaction['iso_currency_code'],
-                "tags": transaction['category']
             })
 
         response = requests.post(
@@ -209,58 +230,47 @@ def insert_transactions(plaid_transactions, firefly_existing_transactions, firef
                 f"Failed to insert transaction '{transaction['name']}'. Status code: {response.status_code}")
 
 
-def match_transactions(client, plaid_config, firefly_config, firefly_existing_transactions):
-    # Code that updates matching transactions, to be run once
-    firefly_ids = {firefly_transaction['attributes']['transactions'][0]['external_id']
-                   for firefly_transaction in firefly_existing_transactions}
-    plaid_transactions = plaid_sync_transactions(client, plaid_config)
-    for transaction in plaid_transactions:
-        if transaction['transaction_id'] in firefly_ids:
-            continue
-
-        matching = find_matching_transactions(
-            firefly_existing_transactions, transaction)
-        if len(matching) == 1:
-            update_existing_transaction_with_id(
-                firefly_config, matching[0], transaction['transaction_id'])
-            continue
-        elif len(matching) > 1:
-            logging.info("Multiple matches found")
-
-
-def sync(plaid_config, firefly_config, client, firefly_existing_transactions):
-    plaid_transactions = plaid_sync_transactions(client, plaid_config)
+def sync(config, accounts, client, firefly_existing_transactions):
+    logging.info("Starting sync...")
+    plaid_transactions = plaid_sync_transactions(client, config)
     insert_transactions(
-        plaid_transactions, firefly_existing_transactions, firefly_config, plaid_config)
+        config, accounts, plaid_transactions, firefly_existing_transactions)
 
 
 def main():
 
+    logging.basicConfig()
+    logging.root.setLevel(logging.INFO)
+
+    logging.info("Starting Plaid to Firefly sync.")
+
+    global cursors
+
     # Read credentials from config file
-    plaid_config, firefly_config = read_config()
+    config, accounts = read_config()
+
+    cursors = [None] * len(config['plaid_access_tokens'])
 
     # Initialize Plaid client
     configuration = plaid.Configuration(
         host=plaid.Environment.Development,
         api_key={
-            'clientId': plaid_config['client_id'],
-            'secret': plaid_config['secret'],
+            'clientId': config['plaid_client_id'],
+            'secret': config['plaid_secret'],
         }
     )
     api_client = plaid.ApiClient(configuration)
     client = plaid_api.PlaidApi(api_client)
 
     firefly_existing_transactions = firefly_get_existing_transactions(
-        firefly_config)
+        config, accounts)
 
-    # match_transactions(client, plaid_config, firefly_config, firefly_existing_transactions)
-
-    sync(plaid_config, firefly_config, client, firefly_existing_transactions)
+    sync(config, accounts, client, firefly_existing_transactions)
 
     # schedule.every(10).minutes.do(
     #     sync,
-    #     plaid_config=plaid_config,
-    #     firefly_config=firefly_config,
+    #     config=config,
+    #     config=config,
     #     client=client,
     #     firefly_existing_transactions=firefly_existing_transactions
     # )
